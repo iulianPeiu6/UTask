ceaa2fe9e479d85d6c949dd9b453324da72dfd89
Iulian Peiu <52739277+iulianPeiu6@users.noreply.github.com>
2021-08-03 16:40:40 +0300

    Update Workspace.cs
    Add properties: Id, Name, Visibility

--- UTaskApp/UTask.Models/Workspace.cs
+++ UTaskApp/UTask.Models/Workspace.cs
@@ -1,6 +1,13 @@
-﻿namespace UTask.Models
+﻿using System;
+
+namespace UTask.Models
 {
     public class Workspace
     {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Visibility { get; set; }
     }
 }
