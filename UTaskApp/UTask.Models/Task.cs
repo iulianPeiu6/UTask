6ece858053a50adcf761ee3bf7f823730d16513e
Iulian Peiu <52739277+iulianPeiu6@users.noreply.github.com>
2021-08-03 16:46:37 +0300

    Update Task.cs
    Add properties: Id, Title, Description, StartDate, DueDate

--- UTaskApp/UTask.Models/Task.cs
+++ UTaskApp/UTask.Models/Task.cs
@@ -1,6 +1,17 @@
-﻿namespace UTask.Models
+﻿using System;
+
+namespace UTask.Models
 {
     public class Task
     {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime DueDate { get; set; }
     }
 }
