89c4f5f627c5d2776f907ab7695434a09cd179a9
Iulian Peiu <52739277+iulianPeiu6@users.noreply.github.com>
2021-08-03 16:26:19 +0300

    Update Settings.cs
    Add properties: Id, NotifyMe, KeepMeLoggedIn

--- UTaskApp/UTask.Models/Settings.cs
+++ UTaskApp/UTask.Models/Settings.cs
@@ -1,6 +1,13 @@
-﻿namespace UTask.Models
+﻿using System;
+
+namespace UTask.Models
 {
     public class Settings
     {
+        public Guid Id { get; set; }
+
+        public bool NotifyMe { get; set; }
+
+        public bool KeepMeLoggedIn { get; set; }
     }
 }
