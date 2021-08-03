555b9dcdc42dc9de169c0ad1e6e7195054911f70
Iulian Peiu <52739277+iulianPeiu6@users.noreply.github.com>
2021-08-03 13:42:30 +0300

    Update User.cs
    Add properties to User model:Id, Username, Password, Email

--- UTaskApp/UTask.Models/User.cs
+++ UTaskApp/UTask.Models/User.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UTask.Models
 {
-    class User
+    public class User
     {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
     }
 }
