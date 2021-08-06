cc262be40f2a2ca9367dfa006f2646d14169e439
Iulian Peiu <52739277+iulianPeiu6@users.noreply.github.com>
2021-08-06 15:27:32 +0300

    Create Infrastructure, Remove Test Console

---
+++ UTask.Services/Infrastructure/DependencyMapper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using UTask.DataAccess.Context;
+using UTask.Services.Users;
+
+namespace UTask.Services.Infrastructure
+{
+    public class DependencyMapper
+    {
+        public static ServiceCollection AddDbContext(ServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<UTaskContext>(options => options.UseSqlServer(connectionString));
+
+            return services;
+        }
+
+        public static ServiceCollection MapDependencies(ServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+
+            return services;
+        }
+    }
+}
